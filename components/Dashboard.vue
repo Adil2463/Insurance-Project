c10901eb6c54d0695645f69eb6b235974c27227f
adilanwar <adilanwar0318@gmail.com>
2026-01-09 18:48:49 +0330

    Make the navbar compo

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -77,22 +77,14 @@ const rows = [{
 <template>
     <div>
         <div class="px-10 pt-3 pb-3.5">
-            <div class="flex justify-between items-center ">
-                <div class="flex items-center gap-8  ">
-                    <div>
-                        <img src="../assets/images/dashboard page/dashboard logo.svg" alt="">
-                    </div>
-                    <div>
-                        <ul class="flex items-center gap-9 ">
-                            <li class="text-[#121714] text-sm font-['Inter'] font-medium not-italic leading-normal cursor-pointer"><a href=""></a>Dashboard</li>
-                            <li class="text-[#121714] text-sm font-['Inter'] font-medium not-italic leading-normal cursor-pointer"><a href=""></a>Reports</li>
-                        </ul>
-                    </div>
-                </div>
-                <div>
-                    <img src="../assets/images/dashboard page/dasshboard img.svg" alt="">
-                </div>
+           
+            <div>
+                 <Navbar />
             </div>
+           
+           
+            
+             
             <div class="mt-6">
                 <p class="text-[#59D19D] font-['Inter'] text-[12px] not-italic font-normal leading-normal">Welcome to <span class="italic">Wiglesworth</span>-Rindom Insurance,</p>
                 <p class="text-[#214869] font-['Inter'] text-2xl font-bold leading-normal not-italic  ">Homeowner Insurance</p>
---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="flex justify-between items-center ">
+        <div class="flex items-center gap-8  ">
+            <div>
+                <img src="../assets/images/dashboard page/dashboard logo.svg" alt="">
+            </div>
+            <div>
+                <ul class="flex items-center gap-9 ">
+                    <li class="text-[#121714] text-sm font-['Inter'] font-medium not-italic leading-normal cursor-pointer"><a href=""></a>Dashboard</li>
+                    <li class="text-[#121714] text-sm font-['Inter'] font-medium not-italic leading-normal cursor-pointer"><a href=""></a>Reports</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <img src="../assets/images/dashboard page/dasshboard img.svg" alt="">
+        </div>
+    </div>
+</template>
